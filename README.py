e244973aa8f8dd30452135543e27703ac709f75f
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-09-28 00:15:53 +0000

    Update README.py

--- README.py
+++ README.py
@@ -3,3 +3,9 @@ import re
 def artihmetic_arranger(problems, solve=False):
   if len(problems) > 5:
     return "Error: Too many problems."
+
+ first_number = ""
+ second_number = ""
+ lines = ""
+ solution = ""
+ string = ""
